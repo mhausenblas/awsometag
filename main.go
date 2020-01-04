47b3171286078102bedaa692aaa17ee2d2c51cc8
Michael Hausenblas <hausenbl@amazon.com>
2020-01-04 12:48:24 +0000

    inits services, advances arg handling
    Signed-off-by: Michael Hausenblas <hausenbl@amazon.com>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws/arn"
 )
@@ -16,16 +17,18 @@ func main() {
 	res := os.Args[1]
 	// a comma-separated list of tags os supposed to be the second argument:
 	tags := os.Args[2]
+	// first try to guess the resource/service type:
 	rtype, err := guesstype(res)
 	if err != nil {
 		log.Fatalf("Can't guess the type of resource based on ARN %s", res)
 	}
-	log.Printf("Tagging %s of type %s with %s", res, rtype, tags)
-	rtag(res, rtype)
+	// and finally try to tag the resource/service with the tags provided:
+	rtag(res, rtype, tags)
 }
 
+// guesstype extracts the resource/service type of the ARN, see also:
+// https://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
 func guesstype(res string) (string, error) {
-	// as per https://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
 	arnres, err := arn.Parse(res)
 	if err != nil {
 		return "", err
@@ -33,14 +36,45 @@ func guesstype(res string) (string, error) {
 	return arnres.Service, nil
 }
 
-func rtag(res, rtype string) error {
-	switch rtype {
-	case "iam":
-		fmt.Println("TAG IAM")
-	case "s3":
-		fmt.Println("TAG S3")
-	default:
-		return fmt.Errorf("Don't know how to tag resources of type %s", rtype)
+// rtag tags a given resource with a certain type with a comma-separated
+// list of tags or fails if it doesn't support the resource type
+func rtag(res, rtype, tags string) (err error) {
+	taglist := expand(tags)
+	for _, tag := range taglist {
+		switch rtype {
+		case "iam":
+			err = tagiamuser(res, tag)
+		case "s3":
+			err = tags3bucket(res, tag)
+		default:
+			return fmt.Errorf("Don't know how to tag resources of type %s", rtype)
+		}
+		if err != nil {
+			return err
+		}
 	}
 	return nil
+}
+
+// expand splits a tag of the form 'key1=val1, key2=val2' into a string slice
+func expand(tags string) []string {
+	raw := strings.Split(tags, ",")
+	clean := []string{}
+	for _, tag := range raw {
+		clean = append(clean, strings.TrimSpace(tag))
+	}
+	return clean
+}
+
+// compart splits a tag of the form key=val into its components and tries to
+// provide sensible values if it fails to do so
+func compart(tag string) (key, val string) {
+	if !strings.Contains(tag, "=") {
+		return "", ""
+	}
+	kv := strings.Split(tag, "=")
+	if len(kv) == 2 {
+		return kv[0], kv[1]
+	}
+	return kv[0], ""
 }
--- services.go
+++ services.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+)
+
+func tags3bucket(arn, tag string) error {
+	k, v := compart(tag)
+	log.Printf("Tagging S3 bucket %s with %s:%s", arn, k, v)
+	return nil
+}
+
+func tagiamuser(arn, tag string) error {
+	k, v := compart(tag)
+	log.Printf("Tagging IAM user %s with %s:%s", arn, k, v)
+	return nil
+}
